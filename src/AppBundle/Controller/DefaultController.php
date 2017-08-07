f7f1b5debdfb3ca5b0c940a011d8c919033db259
root <root@debian>
2017-08-07 14:49:20 +0200

    page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,5 +14,35 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         return $this->render('default/index.html.twig',array());
+    }    
+    /**
+     * @Route("/ofirmie", name="ofirmie")
+     */
+    public function ofirmieAction(Request $request)
+    {
+        return $this->render('default/ofirmie.html.twig',array());
+    }    
+    /**
+     * @Route("/produkty", name="produkty")
+     */
+    public function produktyAction(Request $request)
+    {
+        return $this->render('default/produkty.html.twig',array());
+    }    
+    /**
+     * @Route("/pracownicy", name="pracownicy")
+     */
+    public function pracownicyAction(Request $request)
+    {
+        return $this->render('default/pracownicy.html.twig',array());
+    }    
+    /**
+     * @Route("/kontakt", name="kontakt")
+     */
+    public function kontaktAction(Request $request)
+    {
+        return $this->render('default/kontakt.html.twig',array());
     }
+    
+    
 }
